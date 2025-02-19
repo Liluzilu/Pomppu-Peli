7c89ecf00e809547ee483000921d9b840d706607
Liluzilu <lila.annala@gmail.com>
2025-02-19 17:56:34 +0200

    eka pusku

---
+++ public/script.js
@@ -0,0 +1,30 @@
+let pelinKorkeus;
+let pelinLeveys;
+
+let taustakuva;
+
+let mailanYSijainti;
+let mailanKorkeus = 20;
+let mailanLeveys = 40;
+
+function preload(){
+    taustakuva = loadImage('kuvat/tausta.jpg');
+}
+
+function setup(){
+    pelinLeveys = windowWidth;
+    pelinKorkeus = windowWidth / 3;
+    mailanYSijainti = pelinKorkeus - mailanKorkeus;
+    createCanvas(pelinLeveys,pelinKorkeus);
+}
+function draw(){
+    background("#8a769c");
+    image(taustakuva,0,0,pelinLeveys,pelinKorkeus);
+    piirraMaila();
+    
+}
+
+function piirraMaila(){
+    rect(mouseX,mailanYSijainti,mailanLeveys,mailanKorkeus);
+
+}
